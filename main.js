bf440ae4dfdf6ebe9e446bf84aebe6bb00a8b458
lrouse <lauren.rouse@ingramcontent.com>
2018-09-01 20:40:33 -0500

    Added debugging code exersizes pre-solutions

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log('welcome to debugging');
+
+//// 1st example -- how to troubleshoot error messages, not solved ////
+// const name = "Fred"
+
+// if (nmae === "Fred") {
+//    console.log("Hello Fred")
+// }
+
+
+//// 1st exercise, not solved ////
+// const drinkingAge = 21
+// const age = 35
+
+// if (age < drnikingAge) {
+//     console.log("You can't drink here")
+// } else {
+//     console.log("Have a beer")
+// }
+
+
+//// 2nd example, not solved ////
+// const message = "You're done"
+
+// for (let i = 0; i < 5; i++) {
+//     if (i === 5) {
+//         console.log(mesage)
+//     } else {
+//         console.log(`You are current at ${i}`)
+//     }
+// }
+
+
+//// 2nd exercise -- This code should be console logging each one of the colors on a separate line. not solved ////
+// const colors = ["Red", "Orange", "Periwinkle", "Goldenrod", "Tomato", "Dodger Blue"]
+
+// for (let i = 0; i <= colors.length; i++) {
+//     console.log(colors[0])
+// }
